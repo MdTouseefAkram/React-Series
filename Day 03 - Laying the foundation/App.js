57981dabff60dde9785cc8c5eec732d4d519ce25
Md Touseef Akram <121446983+MdTouseefAkram@users.noreply.github.com>
2024-12-19 09:44:46 +0530

    Update App.js

--- Day 03 - Laying the foundation/App.js
+++ Day 03 - Laying the foundation/App.js
@@ -1,75 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// // React.createElement => ReactElement-JS Object => HTMLElement(render)
-
-// const heading = React.createElement(
-//     "h1",
-//     {id: "heading"},
-//     " Core React"
-// );
-
-// console.log(heading);
-
-// // JSX (transpiled before it reaches the JS) - PARCEL - Babel
-
-// // JSX => React.createElement => ReactElement-JS Object => HTMLElement(render)
-// const jsxHeading = <h1 id="heaidng">React using JSX</h1>;
-
-// console.log(jsxHeading);
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(jsxHeading);
-
-//////////////////////////////////////////////////////////////////////
-
-// React Component
-const heading = (
-    <h1 className="hand" tabIndex="5">
-        React using JSX
-    </h1>
-);
-
-// React Component
-// Class Based Component
-// Functional Component - NEW
-
-
-//React Functional Component
-const HeadingComponent  = () => {
-    return <h1 className="heading">React Functional Component</h1>
-};
-
-const HeadingComponent2 = () => (
-    <h1 className="heading">React Functional Component</h1>
-)
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
-
-//////////////////////////////////////////////////////////////
-
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is React"),
+    React.createElement("h2", {}, "by Md Touseef Akram"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+]);
 const Title = () => (
-    <h1 className="hand" tabIndex="5">
-        React using JSX
-    </h1>
+  <h1 className="head" tabIndex="5">
+    React using JSX 
+  </h1>
 );
 
-const HeadingComponent3 = () => (
+console.log(parent); // object
+// React Fragment - behaves like an empty tag
+const HeadingComponent = () => (
+  <>
     <div id="container">
-        {Title()}
-        <Title/>
-        <Tile></Tile>
-        {/* these three are same thing */}
-        <h1 className="heading">React Functional Component</h1>
+      <Title />
+      <h1 className="heaidng">React Fucntional Component</h1>
     </div>
+    <div id="container-2"></div>
+  </>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent/>);
-
-//we can any javacript code in jsx using {} brackets or can write expression also like
-//{12+13}
+root.render(parent);
+root.render(<HeadingComponent />);
